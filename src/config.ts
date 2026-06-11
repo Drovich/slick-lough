d98f18e194ff434fdf7983717ba7c302140d82f9
AR262832 <alexandre.rapetti@cea.fr>
2026-06-11 15:16:09 +0200

    mise à jours et deploiement sur site

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,10 @@
 // ⬇️ Modifier ce fichier chaque année pour mettre à jour l'édition
 export const EVENT = {
   edition: 6,
+  subtitle: "6e édition de l'évènement estival préféré des français·es",
   date: "1er août 2026",
   lieu: "Mézin (Lot-et-Garonne)",
   adresse: "2990 route de Nérac, Lieu dit Bacala, 47170 Mézin",
   framaformUrl: "https://framaforms.org/slick-lough-1777926479",
-  mapsUrl: "https://www.openstreetmap.org/?mlat=44.0552&mlon=0.0460#map=15/44.0552/0.0460",
+  mapsUrl: "https://www.openstreetmap.org/?mlat=44.08022493124008&mlon=0.27833282947540283#map=19/44.080225/0.278333",
 } as const;
